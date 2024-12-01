305c8ee588f769f460b651ff8dc56abbb5170a0b
EmiAba <abadjieva@gmail.com>
2024-12-01 11:42:12 +0200

    Removed comments
    Removed comments for cleaner and more professional code presentation.

--- wp-content/themes/teahouse/latest-articles.php
+++ wp-content/themes/teahouse/latest-articles.php
@@ -2,10 +2,10 @@
 // Number of posts for "Latest Articles"
 $number_of_posts = 5;
 
-// Query for the first post and the rest
+
 $args = array(
     'post_type'      => 'post',
-    'posts_per_page' => $number_of_posts + 1, // Current Article + Latest Articles
+    'posts_per_page' => $number_of_posts + 1, 
 );
 $query = new WP_Query($args);
 ?>
@@ -22,7 +22,7 @@ $query = new WP_Query($args);
             <!-- Current Article (First Post) -->
             <div class="col-lg-8 wow fadeIn" data-wow-delay="0.1s">
                 <?php if ($query->have_posts()) : ?>
-                    <?php $query->the_post(); // Take the first post ?>
+                    <?php $query->the_post();  ?>
                     <div class="featured-article">
                         <a href="<?php the_permalink(); ?>">
                             <img class="img-fluid mb-3" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="<?php the_title(); ?>" style="border-radius: 10px; max-height: 400px; object-fit: cover; width: 100%;">
@@ -40,7 +40,7 @@ $query = new WP_Query($args);
             <div class="col-lg-4 wow fadeIn" data-wow-delay="0.3s">
                 <div class="recent-articles">
                     <?php if ($query->have_posts()) : ?>
-                        <?php while ($query->have_posts()) : $query->the_post(); // Fetch remaining posts ?>
+                        <?php while ($query->have_posts()) : $query->the_post();  ?>
                             <div class="recent-article mb-4 d-flex align-items-center">
                                 <a href="<?php the_permalink(); ?>">
                                     <img class="img-fluid me-3" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>" alt="<?php the_title(); ?>" style="width: 100px; height: 100px; object-fit: cover; border-radius: 8px;">
@@ -53,7 +53,7 @@ $query = new WP_Query($args);
                                 </div>
                             </div>
                         <?php endwhile; ?>
-                        <?php wp_reset_postdata(); // Reset the query ?>
+                        <?php wp_reset_postdata(); ?>
                     <?php else : ?>
                         <p>No recent articles found.</p>
                     <?php endif; ?>
