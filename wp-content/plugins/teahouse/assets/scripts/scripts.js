fed1094f0fb5b9fa05aff693c82fa2e0cfcf97be
EmiAba <abadjieva@gmail.com>
2024-12-03 14:52:25 +0200

    created custom script for votes

---
+++ wp-content/plugins/teahouse/assets/scripts/scripts.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function ($) {
+    $('.like-button').on('click', function (e) {
+        e.preventDefault();
+
+        let product_id = jQuery(this).attr('data-id'); 
+        let button = jQuery(this); 
+
+        jQuery.ajax({
+            type: 'post',
+            dataType: 'json',
+            url: my_ajax_object.ajax_url, 
+            data: {
+                action: 'teahouse_product_upvote', 
+                product_id: product_id, 
+            },
+            success: function (response) {
+                if (response.success) {
+                   
+                    button.find('.like-count').text(response.data.new_votes);
+                } else {
+                    console.error(response.data.message);
+                }
+            },
+            error: function () {
+                console.error('Something went wrong with the AJAX request.');
+            },
+        });
+    });
+});
+
+/* tea script menu */
+
+/*     function openTeaMenu() {
+        document.getElementById("teaMenu").style.display = "block";
+    }
+
+    function closeTeaMenu() {
+        document.getElementById("teaMenu").style.display = "none";
+    } */
+
